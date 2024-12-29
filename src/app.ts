1a3bd8cf0ee6e6540ee1d968d5b7c318fb4e8cb2
Sanjaee <afrizaahmad18@gmail.com>
2024-12-29 21:48:09 +0700

    test

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,23 @@
+import fastify from "fastify";
+import userRoutes from "./routes/userRoutes";
+import categoryRoutes from "./routes/categoryRoutes";
 
-import fastify from 'fastify';
-import userRoutes from './routes/userRoutes';
-import categoryRoutes from './routes/categoryRoutes';
-
-
-import productRoutes from './routes/productRoutes';
-import { unitRoutes } from './routes/unitRoutes';
-import saleRoutes from './routes/saleRoutes';
-import { transactionRoutes } from './routes/transactionRoute';
+import productRoutes from "./routes/productRoutes";
+import { unitRoutes } from "./routes/unitRoutes";
+import saleRoutes from "./routes/saleRoutes";
+import { transactionRoutes } from "./routes/transactionRoute";
 // import saleRoutes from './routes/saleRoutes';
 
- const app = fastify({ logger: true });
+const app = fastify({ logger: true });
 
-app.register(userRoutes, { prefix: '/users' });
-app.register(categoryRoutes, { prefix: '/categories' });
-app.register(productRoutes, { prefix: '/products' });
-app.register(saleRoutes, { prefix: '/sales' });
-app.register(unitRoutes , {prefix: '/units'})
-app.register(transactionRoutes , {prefix: '/transactions'})
-app.get('/', async (request, reply) => {
-  return { hello: 'world' }
-}
-);
+app.register(userRoutes, { prefix: "/users" });
+app.register(categoryRoutes, { prefix: "/categories" });
+app.register(productRoutes, { prefix: "/products" });
+app.register(saleRoutes, { prefix: "/sales" });
+app.register(unitRoutes, { prefix: "/units" });
+app.register(transactionRoutes, { prefix: "/transactions" });
+app.get("/", async (request: any, reply: any) => {
+  return { hello: "world" };
+});
 
-
-
-
-
-  export default app
+export default app;
